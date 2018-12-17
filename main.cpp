e03b83c69ceeb7ac035b425d82842f67838f5c67
Chang Chaoping <43943805+CrossHand@users.noreply.github.com>
2018-12-17 22:18:53 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include"bTree.h"
+void main()
+{
+	bTree tree1;
+	tree1.insert(2);
+	tree1.insert(4);
+	tree1.insert(3);
+	tree1.insert(1);
+	tree1.insert(5);
+	tree1.insert(6);
+	tree1.insert(7);
+	tree1.insert(8);
+	tree1.insert(9);
+	tree1.insert(10);
+	tree1.insert(11);
+	tree1.insert(12);
+	tree1.insert(13);
+	//tree1.insert(14);
+	//tree1.insert(15);
+
+	tree1.print_tree();
+}
